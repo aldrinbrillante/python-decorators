681ba73ad68216498366edd19a16fc3dfdccb9af
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2020-11-19 11:03:36 -0800

    jackie

--- decorators/4_Nested_Funcs.py
+++ decorators/4_Nested_Funcs.py
@@ -1,5 +1,4 @@
 # repl.it: https://repl.it/@MakeSchool/NestedFunctions#main.py
-
 def print_msg(msg):
     # This is the outer enclosing function
     print("I am in the outer function")
@@ -13,7 +12,31 @@ def print_msg(msg):
 
 
 
-print_msg("Hello")
+# print_msg("Hello")
 
 #The code below will throw an error. Read the error.
-#printer("Hello")
+# printer("Hello")
+
+
+
+# def weather(temp):
+
+#   def say_weather(t):
+#     print(f"Today's temp is: {t}")
+
+
+
+def weather(90):
+
+  def say_weather(t):
+    # prints todays temperature
+    print(f"today's temp is {t}")
+
+  def say_goodnight():
+    print("Good night!")
+
+  say_weather(90)
+
+  say_goodnight()
+
+weather(90)
